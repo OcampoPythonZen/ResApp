c0493d1c12457d67be867d59e1d9d0df0defe887
Edd <edgar.ocampo@gonet.us>
2020-05-24 20:27:11 -0500

    Templates added and db.py added

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 """ We will create the api to use locally and test the url provided by github repository.
 Author : Edgar Ocampo
 """
-from flask import Flask
+import logging
+import os
+from datetime import datetime
+from uuid import uuid4
+
+from flask import Flask, jsonify, render_template
+from db import RESUELVE_FC
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def welcome_home() -> object:
+    return render_template('welcome.html', title='RESUELVE')
+
+
+@app.route('/resuelve_fc')
+def get_players() -> dict:
+    return jsonify(RESUELVE_FC)
 
 
 if __name__ == '__main__':
---
+++ db.py
@@ -0,0 +1,44 @@
+from uuid import uuid4
+
+RESUELVE_FC = [
+    {
+        "uuid": uuid4().hex,
+        "nombre": "Juan Perez",
+        "nivel": "C",
+        "goles": 10,
+        "sueldo": 50000,
+        "bono": 25000,
+        "sueldo_completo": None,
+        "equipo": "rojo"
+    },
+    {
+        "uuid": uuid4().hex,
+        "nombre": "El Cuau",
+        "nivel": "Cuau",
+        "goles": 30,
+        "sueldo": 100000,
+        "bono": 30000,
+        "sueldo_completo": None,
+        "equipo": "azul"
+    },
+    {
+        "uuid": uuid4().hex,
+        "nombre": "Cosme Fulanito",
+        "nivel": "A",
+        "goles": 7,
+        "sueldo": 20000,
+        "bono": 10000,
+        "sueldo_completo": None,
+        "equipo": "azul"
+    },
+    {
+        "uuid": uuid4().hex,
+        "nombre": "El Rulo",
+        "nivel": "B",
+        "goles": 9,
+        "sueldo": 30000,
+        "bono": 15000,
+        "sueldo_completo": None,
+        "equipo": "rojo"
+    }
+]
